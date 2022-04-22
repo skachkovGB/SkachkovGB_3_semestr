71bcfc9db77e3d0be12c8048e2d04a3754861ef5
skachkovGB <skachok2la@gmail.com>
2022-04-22 10:27:57 +0300

    1 lesson
    2-5

--- src/Lesson01/HomeWorkApp.java
+++ src/Lesson01/HomeWorkApp.java
@@ -5,34 +5,59 @@ package Lesson01;
  * Geek Brains 3.1 - Java
  * Lesson 1
  */
-
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor(101);
+        compareNumbers();
     }
-
-
-    // второе задание
-    public static void printThreeWords(){
+    public static void printThreeWords(){            // второе задание
         System.out.println("Orange");
         System.out.print("Banana \n");
         System.out.println("Apple");
+        System.out.print("\n");
     }
 
-    // третье задание
-    public static void checkSumSign(){
+    public static void checkSumSign(){              // третье задание
         int a = 20;
         int b = -21;
         if (a+b>0) {
             System.out.println("Sum numbers " + a + " and " + b + " above zero");
-            System.out.println("Сумма положительная");
+            //System.out.println("Сумма положительная");
         }
         else {
             System.out.println("Sum numbers " + a + " and " + b + " sub zero");
-            System.out.println("Сумма отрицательная");
+            //System.out.println("Сумма отрицательная");
         }
+        System.out.print("\n");
     }
 
 
+    public static void printColor(int value){        // четвертое задание
+        //int value = 0;
+        if (value <= 0) {
+            System.out.println("Red");
+            //System.out.println("Красный");
+        }
+        else if (value > 0 & value <= 100) {
+            System.out.println("Yellow");
+            //System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Green");
+            //System.out.println("Зеленый");
+        }
+        System.out.print("\n");
+    }
+
+    public static void compareNumbers() {        // пятое задание
+    int a = 110;
+    int b = 100;
+    if (a >= b){
+        System.out.println("a >= b");
+    }
+    else
+        System.out.println("a < b");
+    }
 }
