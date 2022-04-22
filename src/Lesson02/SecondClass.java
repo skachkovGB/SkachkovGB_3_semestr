1c5f3f7afed54b0d5696a8bf7c2466bc2309613c
skachkovGB <skachok2la@gmail.com>
2022-04-22 14:05:29 +0300

    Create branch for lesson 2
    Выполнение заданий 1-5 2 урока

---
+++ src/Lesson02/SecondClass.java
@@ -0,0 +1,70 @@
+package Lesson02;
+
+public class SecondClass {
+    public static void main(String[] args) {
+
+        System.out.println("#1:  10<=Summa<=20 is: "+checkNumberZone(-10,21));
+
+        System.out.print("\n");
+        printPositivOrNegativNumber(0);
+
+        System.out.print("\n");
+        System.out.println("#3:  "+ checkPositiveNegativeReverse(-10));
+
+        System.out.print("\n");
+        printStringFewTimes("testPrint",2);
+
+        System.out.print("\n");
+        int year=1500;
+        System.out.println("#5:  God "+year+" visokosnii - "+checkLeapYear(year));
+
+    }
+
+    public static boolean checkNumberZone(int value1, int value2) {             //первое задание
+         if ( (value1+value2)>=10 && (value1+value2)<=20){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public static void printPositivOrNegativNumber(int value){                    //второе задание
+        if (value >= 0) {
+            System.out.println("#2:  Number " + value + " is positive");
+        }
+        else
+            System.out.println("#2:  Number " + value + " is negative");
+    }
+
+    public static boolean checkPositiveNegativeReverse(int value) {                 //третье задание
+        if (value < 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public static void printStringFewTimes(String stringForPrint, int howTimes){    //четвертое задание
+        if (howTimes<1){
+            System.out.println("#4:  Vvedite cheloe chislo ot 1 i bolishe");
+        }
+        else {
+            for (int i=1; i<=howTimes; i++)
+                System.out.println("#4:  "+i+"."+stringForPrint);
+        }
+    }
+
+    public static boolean checkLeapYear (int year){
+
+        boolean div4 = (year % 4 == 0);
+        boolean div100 = (year % 100 == 0);
+        boolean div400 = (year % 400 == 0);
+
+
+        if (div4 && !div100 || div400){
+            return true;
+        }
+        else
+            return false;
+    }
+
+}
