6a5f8688a09c9402954ff5f8b704d51153bd8632
skachkovGB <skachok2la@gmail.com>
2022-05-19 15:48:52 +0300

    8 lesson
    homework

---
+++ src/Lesson08/EighthClass.java
@@ -0,0 +1,15 @@
+package Lesson08;
+
+/**
+ * Created by Skachkov
+ * Date: 12.05.2022
+ * Geek Brains 3.1 - Java
+ * Lesson 8
+ */
+
+
+public class EighthClass {
+    public static void main(String[] args) {
+
+    }
+}
